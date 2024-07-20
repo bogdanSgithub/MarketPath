809e6ccb4588c943f60fe7c4e6efcb6381719742
Bogdan Feher <bogdan.feher@gmail.com>
2024-07-19 20:11:17 -0400

    setup front end and backend for our app

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import pandas as pd
+import yfinance as yf
+import datetime as dt
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+# get list of sp500 tickers
+@app.get("/sp500")
+async def get_sp500():
+    tickers = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]['Symbol'].str.replace('.', '-')
+    return tickers.to_list()
+
+@app.get("/historical_data")
+async def get_historical_data(stock: str, start: str = "2024-01-01", end: str = dt.datetime.now().strftime("%Y-%m-%d"), interval: str = '1d'):
+    start_date = dt.datetime.strptime(start, "%Y-%m-%d")
+    end_date = dt.datetime.strptime(end, "%Y-%m-%d")
+    df = yf.download(stock, start=start_date, end=end_date, interval=interval)
+    return df.reset_index().to_dict(orient='records')
