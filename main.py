552811aec130b056c895c1d98ee86fcdb52e2274
Bogdan Feher <bogdan.feher@gmail.com>
2025-01-09 02:10:27 -0500

    added results at the end of watchlist window

--- main.py
+++ main.py
@@ -37,11 +37,38 @@ end_date = dt.datetime.now()
 pred_start_date = dt.datetime.strptime(df_pred.index[0], "%Y-%m-%d")
 pred_end_date = pred_start_date.replace(year=pred_start_date.year + 1)
 
+
 def get_historical_price(stock: str, start: str = "2023-01-01", end: str = dt.datetime.now().strftime("%Y-%m-%d"), interval: str = '1d'):
     start_date = dt.datetime.strptime(start, "%Y-%m-%d")
     end_date = dt.datetime.strptime(end, "%Y-%m-%d")
     df = yf.download(stock, start=start_date, end=end_date, interval=interval, progress=False)
     return df
+
+
+def get_last_weekday():
+    today = dt.datetime.now()
+
+    # Check if today is Monday (weekday=0) or weekend (Saturday=5, Sunday=6)
+    if today.weekday() == 0:  # Monday
+        last_weekday = today - dt.timedelta(days=3)
+    elif today.weekday() == 6:  # Sunday
+        last_weekday = today - dt.timedelta(days=2)
+    elif today.weekday() == 5:  # Saturday
+        last_weekday = today - dt.timedelta(days=1)
+    else:
+        last_weekday = today - dt.timedelta(days=1)
+    return last_weekday
+
+
+def get_winners():
+    X = df_pred.iloc[:, 2:]
+    predictions = model.predict(X)
+
+    preds = pd.Series(predictions, name='Predictions')
+    merged = pd.concat([df_pred.reset_index(drop=True), preds.reset_index(drop=True)], axis=1)
+
+    df_winners = merged[merged['Predictions'] == True].set_index('Ticker')
+    return df_winners
 
 def Forecast():
     # Get symbol
@@ -142,18 +169,70 @@ def Forecast():
         else:
             st.markdown("# Model's Prediction: :red[HOLD]")
             st.write(f'##### The model predicts {symbol} to **NOT OUTPERFORM** the market by 10% from {pred_start_date.strftime("%Y-%m-%d")} until {pred_end_date.strftime("%Y-%m-%d")}')
-    
+
+
 def Watchlist():
-    X = df_pred.iloc[:, 2:]
-    predictions = model.predict(X)
-
-    preds = pd.Series(predictions, name='Predictions')
-    merged = pd.concat([df_pred.reset_index(drop=True), preds.reset_index(drop=True)], axis=1)
-
-    df_winners = merged[merged['Predictions'] == True].set_index('Ticker')
     st.write("### Model's Watchlist: Stocks Predicted to Beat the Market")
     st.write(f'##### From {pred_start_date.strftime("%Y-%m-%d")} Until {pred_end_date.strftime("%Y-%m-%d")}')
-    st.dataframe(df_winners, width=800, height=878)
+    st.dataframe(get_winners(), width=800, height=878)
+
+    last_weekday = get_last_weekday()
+    st.write(f'### Results From {pred_start_date.strftime("%Y-%m-%d")} Until {last_weekday.strftime("%Y-%m-%d")}')
+
+    df_spy_price = get_historical_price("spy", pred_start_date.strftime("%Y-%m-%d"), last_weekday.strftime("%Y-%m-%d"))
+    fig = go.Figure()
+    fig.add_trace(go.Candlestick(x=df_spy_price.index, open=df_spy_price['Open'], high=df_spy_price['High'],
+                                 low=df_spy_price['Low'], close=df_spy_price['Close']))
+    spy_return, spy_pct_return = df_spy_price['Close'].iloc[-1] - df_spy_price['Close'].iloc[0], ((df_spy_price['Close'].iloc[-1] - df_spy_price['Close'].iloc[0]) / df_spy_price['Close'].iloc[0]) * 100
+    title = f"SPY Historical Price Returns: {spy_pct_return:.2f}% (${spy_return:.2f})"
+    fig.update_layout(height=800, xaxis_rangeslider_visible=False, title=title)
+    st.plotly_chart(fig)
+
+    winners = get_winners()
+
+    portfolio_data = []
+
+    # Loop over each stock and fetch historical prices, then calculate the returns and percentage changes
+    for stock in winners.index:
+        print(stock)
+        st.write(stock)
+        df_stock = get_historical_price(stock, pred_start_date.strftime("%Y-%m-%d"), last_weekday.strftime("%Y-%m-%d"))
+
+        # Calculate the return and percentage return
+        stock_return = df_stock['Close'].iloc[-1] - df_stock['Close'].iloc[0]
+        stock_pct_return = ((df_stock['Close'].iloc[-1] - df_stock['Close'].iloc[0]) / df_stock['Close'].iloc[0]) * 100
+
+        # Add the data to the portfolio_data list
+        portfolio_data.append({
+            'Stock': stock,
+            'Return ($)': stock_return,
+            'Percentage Return (%)': stock_pct_return
+        })
+
+        # Plot the stock's historical price using Plotly
+        fig = go.Figure()
+        fig.add_trace(go.Candlestick(
+            x=df_stock.index, open=df_stock['Open'], high=df_stock['High'],
+            low=df_stock['Low'], close=df_stock['Close']
+        ))
+
+        title = f"{stock} Historical Price Returns: {stock_pct_return:.2f}% (${stock_return:.2f})"
+        fig.update_layout(height=800, xaxis_rangeslider_visible=False, title=title)
+        st.plotly_chart(fig)
+
+    # Create a DataFrame from the portfolio_data list
+    portfolio_df = pd.DataFrame(portfolio_data)
+
+    # Calculate the portfolio's average return and percentage return
+    portfolio_return = portfolio_df['Return ($)'].sum()
+    portfolio_pct_return = (portfolio_return / portfolio_df['Return ($)'].count()) / df_spy_price['Close'].iloc[0] * 100
+
+    # Display the portfolio DataFrame with the individual stock returns and portfolio data
+    st.write(portfolio_df)
+    st.write(f"Portfolio Total Return: ${portfolio_return:.2f}")
+    st.write(f"Portfolio Average Percentage Return: {portfolio_pct_return:.2f}%")
+
+
 
 def Roadmap():
     # Project Overview
@@ -193,11 +272,20 @@ def Roadmap():
     st.write(f"""
     Three datasets are used:
     - **Training/Testing:** 2003-2013 from [Python Programming](https://pythonprogramming.net/data-acquisition-machine-learning/)""")
-    st.write(df_train)
+    columns = [
+        "Ticker", "Market Cap", "Enterprise Value", "Trailing P/E", "Forward P/E", "PEG Ratio",
+        "Price/Sales", "Price/Book", "Enterprise Value/Revenue", "Enterprise Value/EBITDA",
+        "Profit Margin", "Operating Margin", "Return on Assets", "Return on Equity",
+        "Revenue", "Revenue Per Share", "EBITDA", "Net Income Avl to Common", "Diluted EPS",
+        "Total Cash", "Total Cash Per Share", "Total Debt", "Total Debt/Equity",
+        "Current Ratio", "Book Value Per Share", "Beta", "50-Day Moving Average",
+        "200-Day Moving Average", "Avg Vol (3 month)", "Shares Outstanding"
+    ]
+    st.write(df_train[columns])
     st.write(f'- **Testing:** 2017-2018 from the same source')
-    st.write(df_val)
+    st.write(df_val[columns])
     st.write('- **Prediction:** 2024 from [Yahoo Finance](https://ca.finance.yahoo.com/quote/NVDA/)')
-    st.write(df_pred)
+    st.write(df_pred[columns])
 
     # Data Collection and Exploration
     st.subheader("Data Collection and Exploration 🔎")
@@ -246,8 +334,14 @@ if selected == 'Forecast':
     Forecast()
 elif selected == 'Watchlist':
     Watchlist()
+elif selected == 'Roadmap':
+    Roadmap()
 else:
     Roadmap()
+
+
+
+
 
 footer="""<style>
 a:link , a:visited{
@@ -275,6 +369,6 @@ text-align: center;
 <p>Developed by <a style='display: block; text-align: center;' href="https://github.com/bogdanSgithub" target="_blank">Bogdan Andrei Feher</a></p>
 </div>
 """
-st.markdown(footer,unsafe_allow_html=True)
+st.markdown(footer, unsafe_allow_html=True)
 
 # run: streamlit run main.py --theme.base="dark" --theme.primaryColor="#228b22"
